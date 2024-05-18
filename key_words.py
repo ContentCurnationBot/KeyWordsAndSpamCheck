2d3774eb12489c52ae5a107b99f28dd54e987722
kolosya-tuchka <a.kolosya@yandex.ru>
2024-05-19 03:34:56 +0700

    Update keywords check

--- key_words.py
+++ key_words.py
@@ -17,21 +17,9 @@ def normalize_word(word):
     return doc.tokens[0].lemma
 
 
-# Текст для проверки
-text = "Это тексты, которые нужно проверить на наличие ключевых слов."
-
-# Ключевые слова
-keywords = ["ключевое", "слово"]
-
-# Нормализуем текст
-normalized_text = [normalize_word(word) for word in text.split()]
-
-# Нормализуем ключевые слова
-normalized_keywords = [normalize_word(word) for word in keywords]
-
-# Проверяем наличие ключевых слов в нормализованном тексте
-for keyword in normalized_keywords:
-    if keyword in normalized_text:
-        print(f"Ключевое слово '{keyword}' найдено!")
-    else:
-        print(f"Ключевое слово '{keyword}' не найдено.")
+def check_for_keywords(text, keywords):
+    # Нормализуем текст
+    normalized_text = [normalize_word(word) for word in text.split()]
+    # Нормализуем ключевые слова
+    normalized_keywords = [normalize_word(word) for word in keywords]
+    return {keyword: keyword in normalized_text for keyword in normalized_keywords}
